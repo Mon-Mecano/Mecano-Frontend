aa53941a602d30a01b0bb07a0424834736803e56
ItsNatsuh <mohamedriyad_bouzani@um5.ac.ma>
2022-09-27 19:38:10 +0100

    pouzani v2

--- my-app/src/components/GarageDetails/GarageDetails.js
+++ my-app/src/components/GarageDetails/GarageDetails.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import axios from "../Axios/Axios";
 import SimpleImageSlider from "react-simple-image-slider";
+import {AiFillStar} from "react-icons/ai";
 
 const GarageDetails = () => {
   const params = useParams();
@@ -19,14 +20,9 @@ const GarageDetails = () => {
     try {
       const response = await axios.get(`/garage/details/${id}/`);
       setGarage(response.data);
-      setPhoto([
-        {
-          url: `https://immense-tor-96559.herokuapp.com${response.data.garage_pictures[0].picture}`,
-        },
-        {
-          url: "",
-        },
-      ]);
+      setPhoto(
+        response.data.garage_pictures.map((garage) => {return {url: `https://immense-tor-96559.herokuapp.com${garage.picture}`}}),
+      );
     } catch (error) {
       console.log(error);
     }
@@ -34,16 +30,19 @@ const GarageDetails = () => {
 
   return (
     <div>
-      <div className="container mx-auto">
+      <div className="container mx-auto w-3/4">
         <div className="my-4">
           <div>
             <h1 className="font-mono font-bold text-2xl">
               {garage.garage_name}
             </h1>
+            <h2 className="font-mono font-medium text-lg mb-4">
+              {garage.address} - {garage.city}
+            </h2>
           </div>
           <div className="flex justify-between items-center">
             <div>
-              <h2 className="font-mono font-bold text-xl">4.5</h2>
+              <h2 className="flex font-mono font-bold text-xl">⭐4.5</h2>
             </div>
             <div>
               <a href="#">
@@ -69,7 +68,7 @@ const GarageDetails = () => {
         </div>
         <div></div>
       </div>
-      <div className="container mx-auto my-10">
+      <div className="container mx-auto w-3/4 my-10">
         <a
           href="#"
           className="font-sans text-white text-lg font-bold bg-green-700 hover:bg-green-600 px-10 py-5 mt-8 rounded-xl transition-all ease-linear shadow-lg"
